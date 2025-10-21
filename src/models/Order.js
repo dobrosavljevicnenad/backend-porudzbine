9754de3986868836c4b9cfd7b2290046334f1676
ndobrosavljevic <n.dobrosavljevic01@gmail.com>
2025-10-21 23:13:05 +0200

    add tura and quantity

--- src/models/Order.js
+++ src/models/Order.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  tura: Number,
+  quantity: Number,
   contactPlace: String,
   klimaDimensions: String,
   maskDimensions: String,
